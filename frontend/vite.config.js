327546fffc1c9cf69aba4b01c811e61f29e5843a
Mohd Uzaid <mohammeduzaid2@gmail.com>
2026-07-07 18:34:18 +0530

    Done changes in favicon

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -106,20 +106,14 @@ export default defineConfig({
         lang: 'en',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png'
-          },
-          {
-            src: 'maskable-icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable'
           }
         ]
       }
